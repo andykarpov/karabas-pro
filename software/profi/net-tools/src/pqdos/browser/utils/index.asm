dd95102f2b204bd499bc864255e5d531074ca5d2
nihirash <anihirash@gmail.com>
2021-03-18 22:10:58 +0300

    fix

--- software/profi/net-tools/src/pqdos/browser/utils/index.asm
+++ software/profi/net-tools/src/pqdos/browser/utils/index.asm
@@ -5,17 +5,42 @@
 
 MEMORY_REG = #7ffd
 CONFIG_REG = #28B
+CPM_REG = #dffd
 
 turboOff:
+    di
     push bc, af
-    ld c, MEMORY_REG : in a, (c) : or #08 : out (c), a
-    ld c, CONFIG_REG : in a, (c) : and %111111101 : or %000000100 : out (c), a
+    ld bc, MEMORY_REG : in a, (c) : ld (.old7ffd + 1), a : or 16 : out (c), a
+   ld bc, CPM_REG : in a, (c) : ld (.olddffd + 1), a : or 32 : out (c), a
+    ld bc, CONFIG_REG : ld a, 96: out (c), a
+    ld bc, MEMORY_REG 
+.old7ffd    
+    ld a, 0
+    out (c), a
+    
+    ld bc, CPM_REG 
+.olddffd
+    ld a, 0
+    out (c), a
+
     pop af, bc
+    ei
     ret
 
 turboOn:
+    di
     push bc, af
-    ld c, MEMORY_REG : in a, (c) : or #08 : out (c), a
-    ld c, CONFIG_REG : in a, (c) : and %111111011 : or %000000010 : out (c), a
+    ld bc, MEMORY_REG : in a, (c)  : ld (.old7ffd + 1), a : or 16 : out (c), a
+    ld bc, CPM_REG : in a, (c) : ld (.olddffd + 1), a : or 32 : out (c), a
+    ld bc, CONFIG_REG : ld a, 64 : out (c), a
+    ld bc, MEMORY_REG 
+.old7ffd    
+    ld a, 0
+    out (c), a
+    ld bc, CPM_REG 
+.olddffd
+    ld a, 0
+    out (c), a
     pop af, bc
+    ei
     ret
