4e90a0c1051a0bc0a06784ff0e39be146b26f040
Andy Karpov <andy.karpov@gmail.com>
2021-05-08 12:03:49 +0300

    new build dir structure

--- firmware/src/build.sh
+++ firmware/src/build.sh
@@ -10,19 +10,19 @@ cd avr
 pio run -t clean
 export PLATFORMIO_BUILD_FLAGS="-DUSE_HW_BUTTONS=0 -DMOUSE_POLL_TYPE=0 -DALLOW_LED_OVERRIDE=1 -Wall"
 pio run
-cp .pio/build/ATmega328/firmware.hex ../../releases/profi/karabas_pro_revA.hex
+cp .pio/build/ATmega328/firmware.hex ../../releases/profi/avr/karabas_pro_revA.hex
 
 # hw buttons + disabled led override
 pio run -t clean
 export PLATFORMIO_BUILD_FLAGS="-DUSE_HW_BUTTONS=1 -DMOUSE_POLL_TYPE=0 -DALLOW_LED_OVERRIDE=0 -Wall"
 pio run
-cp .pio/build/ATmega328/firmware.hex ../../releases/profi/karabas_pro_revD.hex
+cp .pio/build/ATmega328/firmware.hex ../../releases/profi/avr/karabas_pro_revD.hex
 
 # hw buttons
 pio run -t clean
 export PLATFORMIO_BUILD_FLAGS="-DUSE_HW_BUTTONS=1 -DMOUSE_POLL_TYPE=0 -DALLOW_LED_OVERRIDE=1 -Wall"
 pio run
-cp .pio/build/ATmega328/firmware.hex ../../releases/profi/karabas_pro.hex
+cp .pio/build/ATmega328/firmware.hex ../../releases/profi/avr/karabas_pro.hex
 cp .pio/build/ATmega328/firmware.hex ../../releases/pong/karabas_pro.hex
 cp .pio/build/ATmega328/firmware.hex ../../releases/rk86/karabas_pro.hex
 
@@ -34,24 +34,43 @@ echo "Done"
 
 cd ..
 
-echo "Building Profi FPGA sources"
+echo "Building Profi FPGA sources for EP4CE6"
 
-cd fpga/profi/syn
+cd fpga/profi/syn/ep4ce6
 
 make clean
 make all
 make jic
 
-cp karabas_pro_tda1543.jic ../../../../releases/profi/karabas_pro_tda1543.jic
-cp karabas_pro_tda1543a.jic ../../../../releases/profi/karabas_pro_tda1543a.jic
-cp karabas_pro.rbf ../../../../releases/profi/karabas_pro.rbf
-cp karabas_pro.sof ../../../../releases/profi/karabas_pro.sof
+cp karabas_pro_ep4ce6_tda1543.jic ../../../../../releases/profi/ep4ce6/karabas_pro_ep4ce6_tda1543.jic
+cp karabas_pro_ep4ce6_tda1543a.jic ../../../../../releases/profi/ep4ce6/karabas_pro_ep4ce6_tda1543a.jic
+cp karabas_pro_ep4ce6.rbf ../../../../../releases/profi/ep4ce6/karabas_pro_ep4ce6.rbf
+cp karabas_pro_ep4ce6.sof ../../../../../releases/profi/ep4ce6/karabas_pro_ep4ce6.sof
 
 make clean
 
 echo "Done"
 
-cd ../../../
+cd ../../../../
+
+echo "Building Profi FPGA sources for EP4CE10"
+
+cd fpga/profi/syn/ep4ce10
+
+make clean
+make all
+make jic
+
+cp karabas_pro_ep4ce10_tda1543.jic ../../../../../releases/profi/ep4ce10/karabas_pro_ep4ce10_tda1543.jic
+cp karabas_pro_ep4ce10_tda1543a.jic ../../../../../releases/profi/ep4ce10/karabas_pro_ep4ce10_tda1543a.jic
+cp karabas_pro_ep4ce10.rbf ../../../../../releases/profi/ep4ce10/karabas_pro_ep4ce10.rbf
+cp karabas_pro_ep4ce10.sof ../../../../../releases/profi/ep4ce10/karabas_pro_ep4ce10.sof
+
+make clean
+
+echo "Done"
+
+cd ../../../../
 
 echo "Building CPLD sources"
 
@@ -60,7 +79,7 @@ cd cpld/syn
 make clean
 make all
 
-cp karabas_pro_cpld.pof ../../../releases/profi/karabas_pro_cpld.pof
+cp karabas_pro_cpld.pof ../../../releases/profi/epm3128/karabas_pro_cpld.pof
 cp karabas_pro_cpld.pof ../../../releases/pong/karabas_pro_cpld.pof
 cp karabas_pro_cpld.pof ../../../releases/rk86/karabas_pro_cpld.pof
 
