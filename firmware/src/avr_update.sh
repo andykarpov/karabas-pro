8326c2f2fd633a94b3a74127a91c64db141ac670
Andy Karpov <andy.karpov@gmail.com>
2021-05-30 14:28:50 +0300

    added script to build avr only release

---
+++ firmware/src/avr_update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+export PATH=/opt/altera/quartus/bin:$HOME/.platformio/penv/bin:$PATH
+
+echo "Building AVR sources"
+
+cd avr
+
+# without hw buttons
+pio run -t clean
+export PLATFORMIO_BUILD_FLAGS="-DUSE_HW_BUTTONS=0 -DMOUSE_POLL_TYPE=0 -DALLOW_LED_OVERRIDE=1 -Wall"
+pio run
+cp .pio/build/ATmega328/firmware.hex ../../releases/profi/avr/karabas_pro_revA.hex
+
+# hw buttons + disabled led override
+pio run -t clean
+export PLATFORMIO_BUILD_FLAGS="-DUSE_HW_BUTTONS=1 -DMOUSE_POLL_TYPE=0 -DALLOW_LED_OVERRIDE=0 -Wall"
+pio run
+cp .pio/build/ATmega328/firmware.hex ../../releases/profi/avr/karabas_pro_revD.hex
+
+# hw buttons
+pio run -t clean
+export PLATFORMIO_BUILD_FLAGS="-DUSE_HW_BUTTONS=1 -DMOUSE_POLL_TYPE=0 -DALLOW_LED_OVERRIDE=1 -Wall"
+pio run
+cp .pio/build/ATmega328/firmware.hex ../../releases/profi/avr/karabas_pro.hex
+cp .pio/build/ATmega328/firmware.hex ../../releases/pong/karabas_pro.hex
+cp .pio/build/ATmega328/firmware.hex ../../releases/rk86/karabas_pro.hex
+
+pio run -t clean
+
+echo "Done"
+
+cd ..
