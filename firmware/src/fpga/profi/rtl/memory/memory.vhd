18a64365dff412b1a8b34585c083f5d3e6f07101
Andy Karpov <andy.karpov@gmail.com>
2021-07-04 20:45:13 +0300

    initial refactoring to use a dedicated vram

--- firmware/src/fpga/profi/rtl/memory/memory.vhd
+++ firmware/src/fpga/profi/rtl/memory/memory.vhd
@@ -33,7 +33,8 @@ port (
 	N_OE 			: out std_logic;
 	
 	MA 			: out std_logic_vector(20 downto 0);
-	MD 			: inout std_logic_vector(7 downto 0);
+	MDI 			: in std_logic_vector(7 downto 0);
+	MDO 			: out std_logic_vector(7 downto 0);
 	N_MRD 		: out std_logic;
 	N_MWR 		: out std_logic;
 	
@@ -44,14 +45,14 @@ port (
 	
 	VA				: in std_logic_vector(13 downto 0);
 	VID_PAGE 	: in std_logic := '0';
+	VID_DO 		: out std_logic_vector(7 downto 0);
+	VID_BANK		: in std_logic := '0';
+
 	DS80			: in std_logic := '0';
 	CPM 			: in std_logic := '0';
 	SCO			: in std_logic := '0';
 	SCR 			: in std_logic := '0';
 	WOROM 		: in std_logic := '0';
-	
-	VBUS_MODE_O : out std_logic;
-	VID_RD_O : out std_logic;
 	
 	ROM_BANK : in std_logic := '0';
 	EXT_ROM_BANK : in std_logic_vector(1 downto 0) := "00"
@@ -60,53 +61,54 @@ end memory;
 
 architecture RTL of memory is
 
-	signal buf_md		: std_logic_vector(7 downto 0) := "11111111";
-	signal is_buf_wr	: std_logic := '0';	
-	
 	signal is_rom : std_logic := '0';
 	signal is_ram : std_logic := '0';
 	
 	signal rom_page : std_logic_vector(1 downto 0) := "00";
 	signal ram_page : std_logic_vector(6 downto 0) := "0000000";
 
-	signal vbus_req	: std_logic := '1';
-	signal vbus_mode	: std_logic := '1';	
-	signal vbus_rdy	: std_logic := '1';
-	signal vbus_ack 	: std_logic := '1';
-	signal vid_rd : std_logic;
+	signal vid_wr 		: std_logic;
+	signal vid_scr 	: std_logic;
 	
 	signal mux : std_logic_vector(1 downto 0);
 
 begin
 
+	U_VRAM: entity work.altram1
+	port map(
+		clock_a => CLK2X,
+		clock_b => CLK2X,
+		address_a => VID_BANK & vid_scr & A(12 downto 0),
+		address_b => VID_BANK & VID_PAGE & VA(12 downto 0),
+		data_a => D,
+		data_b => "11111111",
+		q_a => open,
+		q_b => VID_DO,
+		wren_a => vid_wr,
+		wren_b => '0'
+	);
+
 	is_rom <= '1' when N_MREQ = '0' and A(15 downto 14)  = "00" and WOROM = '0' else '0';
 	is_ram <= '1' when N_MREQ = '0' and is_rom = '0' else '0';
 	
+	vid_wr <= '1' when N_MREQ = '0' and N_WR = '0' and (ram_page = "0000101" or ram_page = "0000111" ) and A(13) = '0' else '0';
+	vid_scr <= '1' when ram_page = "0000111" and A(13) = '0' else '0';
+		
 	-- 00 - bank 0, CPM
 	-- 01 - bank 1, TRDOS
 	-- 10 - bank 2, Basic-128
 	-- 11 - bank 3, Basic-48
 	rom_page <= (not(TRDOS)) & ROM_BANK;
 		
-	vbus_req <= '0' when ( N_MREQ = '0' or N_IORQ = '0' ) and ( N_WR = '0' or N_RD = '0' ) else '1';
-	vbus_rdy <= '0' when (CLKX = '0' or CLK_CPU = '0')  else '1';
-
-	VBUS_MODE_O <= vbus_mode;
-	VID_RD_O <= vid_rd;
-	
 	N_MRD <= '1' when loader_act = '1' else 
 				'0' when (is_rom = '1' and N_RD = '0') or
-							(vbus_mode = '1' and vbus_rdy = '0') or 
-							(vbus_mode = '0' and N_RD = '0' and N_MREQ = '0') 
+							(N_RD = '0' and N_MREQ = '0') 
 				else '1';
 
 	N_MWR <= not loader_ram_wr when loader_act = '1' else 
-				'0' when vbus_mode = '0' and is_ram = '1' and N_WR = '0' and CLK_CPU = '0' 
-				else '1';
+				'0' is_ram = '1' and N_WR = '0' else '1';
 
-	is_buf_wr <= '1' when vbus_mode = '0' and CLK_CPU = '0' else '0';
-	
-	DO <= buf_md;
+	DO <= MDI;
 	
 	N_OE <= '0' when (is_ram = '1' or is_rom = '1') and N_RD = '0' else '1';
 		
@@ -137,46 +139,18 @@ begin
 		
 	MA(13 downto 0) <= 
 		loader_ram_a(13 downto 0) when loader_act = '1' else -- loader ram
-		A(13 downto 0) when vbus_mode = '0' else -- spectrum ram 
-		VA; -- video ram (read by video controller)
+		A(13 downto 0); -- spectrum ram 
 
 	MA(20 downto 14) <= 
 		loader_ram_a(20 downto 14) when loader_act = '1' else -- loader ram
 		"100" & EXT_ROM_BANK(1 downto 0) & rom_page(1 downto 0) when is_rom = '1' and vbus_mode = '0' else -- rom from sram high bank 
 		ram_page(6 downto 0) when vbus_mode = '0' else 
-		"00001" & VID_PAGE & '1' when vbus_mode = '1' and DS80 = '0' else -- spectrum screen
-		"00001" & VID_PAGE & '0' when vbus_mode = '1' and DS80 = '1' and vid_rd = '0' else -- profi bitmap 
-		"01110" & VID_PAGE & '0' when vbus_mode = '1' and DS80 = '1' and vid_rd = '1' else -- profi attributes
 		"0000000";
 	
-	MD(7 downto 0) <= 
+	MDO(7 downto 0) <= 
 		loader_ram_do when loader_act = '1' else -- loader DO
-		D(7 downto 0) when vbus_mode = '0' and ((is_ram = '1' or (N_IORQ = '0' and N_M1 = '1')) and N_WR = '0') else 
+		D(7 downto 0) when (is_ram = '1' and N_WR = '0') else 
 		(others => 'Z');
-		
-	-- fill memory buf
-	process(is_buf_wr)
-	begin 
-		if (is_buf_wr'event and is_buf_wr = '0') then  -- high to low transition to lattch the MD into BUF
-			buf_md(7 downto 0) <= MD(7 downto 0);
-		end if;
-	end process;	
-	
-	process( CLK2X, CLKX, vbus_mode, vbus_req, vbus_ack )
-	begin
-		-- lower edge of 14 mhz clock
-		if CLK2X'event and CLK2X = '1' then 
-			if (CLKX = '0') then
-				if vbus_req = '0' and vbus_ack = '1' then
-					vbus_mode <= '0';
-				else
-					vbus_mode <= '1';
-					vid_rd <= not vid_rd;
-				end if;	
-				vbus_ack <= vbus_req;
-			end if;		
-		end if;		
-	end process;
 			
 end RTL;
 
