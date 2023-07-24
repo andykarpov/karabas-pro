6596d78011f2590792107de8c1b78ab56cb16a6c
Caasper911 <chastookhin@gmail.com>
2023-07-25 00:22:42 +0300

    quick fix contension issues with Nemo HDD ports in CLASSIC video mode

--- firmware/src/fpga/profi/rtl/karabas_pro.vhd
+++ firmware/src/fpga/profi/rtl/karabas_pro.vhd
@@ -1199,7 +1199,8 @@ cpu_wait_n <= '1';
 -- max turbo = 14 MHz
 max_turbo <= "10";
 
-clk_cpu <= '0' when kb_wait = '1' or  (kb_screen_mode = "01" and memory_contention = '1' and automap = '0' and DS80 = '0') or WAIT_IO = '0' else 
+--OCH: automap = '0' and cs_nemo_ports = '0' - not contend DIVMMC and NEMO ports in CLASSIC screen mode
+clk_cpu <= '0' when kb_wait = '1' or  (kb_screen_mode = "01" and memory_contention = '1' and automap = '0' and cs_nemo_ports = '0' and DS80 = '0') or WAIT_IO = '0' else 
 	clk_bus when turbo_mode = "11" and turbo_mode <= max_turbo else 
 	clk_bus and ena_div2 when turbo_mode = "10" and turbo_mode <= max_turbo else 
 	clk_bus and ena_div4 when turbo_mode = "01" and turbo_mode <= max_turbo else 
